fa4a8fa11452e325229fd1e0e409c8ef3daf6cfa
khue <luumsk@gmail.com>
2025-11-02 16:20:27 +0700

    expose more functions and add new config for 10K images training

--- brainfm/data/__init__.py
+++ brainfm/data/__init__.py
@@ -1,2 +1,2 @@
-from .loader import build_loader
+from .loader import build_loader, mri_collate_fn
 from .dataset import MultiModMRIDataset
--- brainfm/models/__init__.py
+++ brainfm/models/__init__.py
@@ -1,3 +1,3 @@
-from .mae_vit3d import build_model
-from .encoder import build_modality_encoder
+from .mae_vit3d import build_model, load_model_base
+from .encoder import build_modality_encoder, ModalityEncoder
 from .patching import patchify_3d, unpatchify_3d
